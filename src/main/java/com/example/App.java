c366077941790afcd2ef80514f875457fac87bfd
Kiranmaie Puvvula <kiranmaipuvulla@gmail.com>
2025-10-04 13:39:19 +0530

    Update App.java print message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
          System.out.println("Hello, DevOps with Maven!");
+         Syetem.out.println("Hello again");
     }
 }
